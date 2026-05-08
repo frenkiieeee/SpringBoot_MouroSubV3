98f9dbb61ca64a0cbc8f08f384e359b81c71326b
Alvaro180807 <anasyalvaros@gmail.com>
2026-05-08 12:24:17 +0200

    Repositorio de Alquiler, material y seguros, errores correguidos a la hora de las relaciones

---
+++ src/main/java/com/mourosub/web/model/Seguros.java
@@ -0,0 +1,78 @@
+package com.mourosub.web.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "seguro")
+
+public class Seguros {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idSeguro;
+
+    @Column (nullable = false)
+    private Boolean activo = false;
+
+    @Column (nullable = false, length = 100)
+    private String nombre;
+
+    @Column (nullable = false, length = 100)
+    private String compania;
+
+    @Column (name = "cobertura_gastos", nullable = false)
+    private Double coberturaGastos;
+
+    @Column (columnDefinition = "TEXT")
+    private String descripcion;
+
+
+    public Long getIdSeguro() {
+        return idSeguro;
+    }
+
+    public void setIdSeguro(Long idSeguro) {
+        this.idSeguro = idSeguro;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCompania() {
+        return compania;
+    }
+
+    public void setCompania(String compania) {
+        this.compania = compania;
+    }
+
+    public Double getCoberturaGastos() {
+        return coberturaGastos;
+    }
+
+    public void setCoberturaGastos(Double coberturaGastos) {
+        this.coberturaGastos = coberturaGastos;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
+
