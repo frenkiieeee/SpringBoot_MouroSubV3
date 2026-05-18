efa46d27560f3397ffffb0286c870042f94b0469
Alvaro180807 <anasyalvaros@gmail.com>
2026-05-18 10:45:09 +0200

    update: añadi el DTO de seguros y modifique un poco el controller

---
+++ src/main/java/com/mourosub/web/dto/SegurosFormDTO.java
@@ -0,0 +1,78 @@
+package com.mourosub.web.dto;
+
+import java.time.LocalDate;
+
+public class SegurosFormDTO {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String dni;
+    private String direccion;
+    private String codPostal;
+    private String localidad;
+    private LocalDate fechaNacimiento;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodPostal() {
+        return codPostal;
+    }
+
+    public void setCodPostal(String codPostal) {
+        this.codPostal = codPostal;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
